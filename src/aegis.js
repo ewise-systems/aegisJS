39b7d16be2c64a556d745a496586476298f123b8
Rene Calunsag Jr <rcalunsag@ewise.com>
2019-06-17 12:43:15 +0800

    Removed lib and ota functions to separate repo

--- src/aegis.js
+++ src/aegis.js
@@ -1,8 +1,7 @@
-const uniqid = require("uniqid");
 const { BehaviorSubject } = require("rxjs");
-const { toObservable } = require("../lib/frpcore/transforms");
-const { requestToAegisWithToken } = require("../lib/hof/requestToAegis");
-const { kickstart$: createPollingStream } = require("../lib/hos/pollingCore");
+const { toObservable } = require("@ewise/aegisjs-core/frpcore/transforms");
+const { requestToAegisWithToken } = require("@ewise/aegisjs-core/hof/requestToAegis");
+const { kickstart$: createPollingStream } = require("@ewise/aegisjs-core/hos/pollingCore");
 
 const HTTP_VERBS = {
     GET: "GET",
@@ -14,11 +13,6 @@ const HTTP_VERBS = {
 const PDV_PATHS = {
     GET_DETAILS: "/",
     RUN_BROWSER: "/public/browser",
-    GET_INSTITUTIONS: (instCode = "") => `/ota/institutions/${instCode}`,
-    START_OTA: "/ota/process",
-    QUERY_OTA: (pid, csrf = "") => `/ota/process/${pid}?challenge=${csrf}`,
-    RESUME_OTA: (pid = "") => `/ota/process/${pid}`,
-    STOP_OTA: (pid, csrf = "") => `ota/process/${pid}?challenge=${csrf}`,
     ADD_PROFILE: "/profiles",
     GET_PROFILES: (pid = "", cred = false) => `/profiles/${pid}${cred ? "/credential" : ""}`,
     GET_PROCESS: (pid) => `/processes/${pid}`,
@@ -51,47 +45,6 @@ const aegis = (options = {}) => {
                 path: PDV_PATHS.RUN_BROWSER,
                 tokenOrUrl: jwt
             }),
-
-        getInstitutions: (instCode, jwt = defaultJwt) =>
-            requestToAegisWithToken({
-                method: HTTP_VERBS.GET,
-                jwt,
-                body: null,
-                path: PDV_PATHS.GET_INSTITUTIONS(instCode),
-                tokenOrUrl: jwt
-            }),
-
-        initializeOta: (instCode, prompts, jwt = defaultJwt) => {
-            const subject$ = new BehaviorSubject({ value: null });
-
-            const csrf = uniqid();
-            const bodyCsrf = { code: instCode, prompts, challenge: csrf };
-
-            const initialStream$ = toObservable(HTTP_VERBS.POST, jwt, bodyCsrf, PDV_PATHS.START_OTA);
-            const pollingStream$ = pid => toObservable(HTTP_VERBS.GET, jwt, null, PDV_PATHS.QUERY_OTA(pid, csrf));
-            const stream$ = createPollingStream(stopStreamCondition, initialStream$, pollingStream$);
-
-            return {
-                run: () =>
-                    stream$.subscribe(subject$) && subject$,
-                resume: otp =>
-                    requestToAegisWithToken({
-                        method: HTTP_VERBS.POST,
-                        jwt,
-                        body: { ...otp, challenge: csrf },
-                        path: PDV_PATHS.RESUME_OTA(subject$.value.processId),
-                        tokenOrUrl: jwt
-                    }),
-                stop: () =>
-                    requestToAegisWithToken({
-                        method: HTTP_VERBS.DELETE,
-                        jwt,
-                        body: null,
-                        path: PDV_PATHS.STOP_OTA(subject$.value.processId, csrf),
-                        tokenOrUrl: jwt
-                    })
-            };
-        },
 
         addProfile: (instCode, prompts, jwt = defaultJwt) => {
             const subject$ = new BehaviorSubject({ value: null });
